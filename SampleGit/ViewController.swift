91fcad1df6aac5fb8451112786981aab80cb501e
laxman <laxman@rizecorp.com>
2015-10-16 12:07:27 +0530

    Second commit

---
+++ SampleGit/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  SampleGit
+//
+//  Created by ramakrishna on 10/15/15.
+//  Copyright (c) 2015 rize. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        println("My First Git hub demo")
+        println("My Second git hub demo")
+        
+        println("My Second git hub demo")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
